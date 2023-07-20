689f68a7c26bf4f2b1a490632f137a79c5bf1f69
al-Reza89 <alialreza98@gmail.com>
2023-07-20 08:21:39 +0600

    database create

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,8 @@ import getCurrentUser from "./actions/getCurrentUser";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Airbnb",
-  description: "Airbnb clone",
+  title: "e-commerce",
+  description: "Mechanical keyboard",
 };
 
 export default async function RootLayout({
