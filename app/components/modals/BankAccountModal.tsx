5efee4c8d98cf74f22213a141c19cf7461462a78
al-Reza89 <alialreza98@gmail.com>
2023-07-21 10:05:21 +0600

    secret is create in backend

--- app/components/modals/BankAccountModal.tsx
+++ app/components/modals/BankAccountModal.tsx
@@ -1,20 +1,81 @@
 "use client";
 
 import React, { useState } from "react";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+
 import Modal from "./Modal";
 import useBankAccountModal from "@/app/hooks/useBankAccountModal";
+import Input from "../inputs/Input";
+import Heading from "../Heading";
+import axios from "axios";
+import { toast } from "react-hot-toast";
+import { useRouter } from "next/navigation";
 
 interface BankAccoutModalProps {}
 
 const BankAccountModal: React.FC<BankAccoutModalProps> = ({}) => {
   const bankModal = useBankAccountModal();
+  const router = useRouter();
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<FieldValues>({
+    defaultValues: {
+      secret: "",
+    },
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(true);
+
+    axios
+      .post("/api/bank", data)
+      .then(() => {
+        toast.success("successfully create secret");
+        router.refresh();
+        reset();
+        bankModal.onClose();
+      })
+      .catch(() => {
+        toast.error("Something went wrong!");
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
+
+  const bodyContent = (
+    <div>
+      <Heading
+        title="make it strong!!"
+        subtitle="you have to memorise the bank sectet"
+        center={false}
+      />
+      <Input
+        id="secret"
+        label="secret key"
+        disabled={isLoading}
+        errors={errors}
+        register={register}
+        required
+      />
+    </div>
+  );
 
   return (
     <Modal
+      disabled={isLoading}
       isOpen={bankModal.isOpen}
       onClose={bankModal.onClose}
-      onSubmit={() => {}}
+      onSubmit={handleSubmit(onSubmit)}
+      actionLebel="create bank secret"
       title="bank account"
+      body={bodyContent}
     />
   );
 };
