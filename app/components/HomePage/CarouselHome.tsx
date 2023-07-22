274743c66ea0f7fd03add17475ec6c156c708c82
al-Reza89 <alialreza98@gmail.com>
2023-07-22 12:18:33 +0600

    carosel without auto play

---
+++ app/components/HomePage/CarouselHome.tsx
@@ -0,0 +1,75 @@
+/* eslint-disable @next/next/no-img-element */
+"use client";
+
+import React, { useState } from "react";
+import { BsChevronCompactLeft, BsChevronCompactRight } from "react-icons/bs";
+import { RxDotFilled } from "react-icons/rx";
+
+const CarouselHome = () => {
+  const slides = [
+    {
+      url: "mecha1.jpg",
+    },
+    {
+      url: "mecha2.jpg",
+    },
+    {
+      url: "mecha3.jpg",
+    },
+
+    {
+      url: "mecha4.jpg",
+    },
+    {
+      url: "mecha5.jpg",
+    },
+  ];
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const prevSlide = () => {
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const nextSlide = () => {
+    const isLastSlide = currentIndex === slides.length - 1;
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const goToSlide = (slideIndex: any) => {
+    setCurrentIndex(slideIndex);
+  };
+
+  return (
+    <div className="w-screen h-[550px]  m-auto  relative group">
+      <div
+        style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
+        className="w-full h-full bg-center bg-cover duration-500"
+      ></div>
+      {/* Left Arrow */}
+      <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
+        <BsChevronCompactLeft onClick={prevSlide} size={30} />
+      </div>
+      {/* Right Arrow */}
+      <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
+        <BsChevronCompactRight onClick={nextSlide} size={30} />
+      </div>
+      <div className="flex top-4 justify-center py-2">
+        {slides.map((slide, slideIndex) => (
+          <div
+            key={slideIndex}
+            onClick={() => goToSlide(slideIndex)}
+            className="text-2xl cursor-pointer"
+          >
+            <RxDotFilled />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default CarouselHome;
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 import Image from "next/image";
 import Container from "./components/Container";
+import CarouselHome from "./components/HomePage/CarouselHome";
 
 export default function Home() {
   return (
-    <Container>
-      <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8 ">
-        home page
-      </div>
-    </Container>
+    <div className="pt-20">
+      <CarouselHome />
+      <div className="pt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8 "></div>
+    </div>
   );
 }
